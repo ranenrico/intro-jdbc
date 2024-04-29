9462c47e731174ab5d4878fe07625e105564acaf
Enrico Ranieri <ran.enrico00@gmail.com>
2024-04-29 12:53:29 +0200

    first commit

---
+++ src/main/java/org/generation/italy/Main.java
@@ -0,0 +1,34 @@
+package org.generation.italy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.postgresql.Driver;
+
+//perché il mio codice non usa direttamente le classi della libreria di postgres?
+//qual è la tecnica che mi permette di usare delle interfacce per parlare con degli oggetti della libreria di postgres
+
+
+//com'è possibile che il metodo DriverManager getConnection() mi ritorni un oggetto che io non ho nominato?
+
+//perché il DriverManager seleziona il giusto driver in base all'URL (driver ha metodo acceptsURL)
+
+//il metodo DriverManager getConnection() è un esempio di un idioma che si chiama..? (un pattern meno dignitoso)
+
+//factory method
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        final String jdbcurl = "jdbc:postgresql://localhost:5432/company";
+        final String user = "postgresMaster";
+        final String password = "goPostgresGo";
+        try (Connection conn = DriverManager.getConnection(jdbcurl, user, password)) {
+            System.out.println("Connessione riuscita");
+            System.out.println(conn.getClass().getName());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
