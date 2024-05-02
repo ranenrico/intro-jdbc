fdfdf4c1ff306bf8d3ee627754f53f4b2adb6f6d
Sonia Mammone <sonia.mammone95@gmail.com>
2024-05-02 11:58:02 +0200

    commit prima del marge

--- src/main/java/org/generation/italy/introjdbc/model/repositories/JdbcCustomerRepositery.java
+++ src/main/java/org/generation/italy/introjdbc/model/repositories/JdbcCustomerRepositery.java
@@ -19,26 +19,33 @@ public class JdbcCustomerRepositery implements CustomerRepositery{
     private static final String ALL_CUSTOMER = """
                                                     SELECT custid, companyname, contactname, 
                                                     contacttitle, adress, city, region, postalcode, 
-                                                    country, phone, fax FROM customers
+                                                    country, phone, fax 
+                                                    FROM customers
                                                     """;
     private static final String ALL_CUSTOMER_NAME_LIKE = """
-                                                                SELECT categoryid, categoryname, description 
-                                                                FROM categories
-                                                                WHERE categoryname LIKE ?
-                                                                """;
+                                                            SELECT custid, companyname, contactname, 
+                                                            contacttitle, adress, city, region, postalcode, 
+                                                            country, phone, fax FROM customers 
+                                                            FROM customers
+                                                            WHERE companyname LIKE ?
+                                                            """;
     private static final String CUSTOMER_BY_ID = """
-                                                    SELECT categoryid, categoryname, description 
-                                                    FROM categories
-                                                    WHERE categoryid = ?
+                                                    SELECT custid, companyname, contactname, 
+                                                    contacttitle, adress, city, region, postalcode, 
+                                                    country, phone, fax 
+                                                    FROM customers
+                                                    WHERE custid = ?
                                                     """;
     private static final String DELETE_BY_ID = """
-                                                    DELETE FROM categories
-                                                    WHERE categoryid = ?
+                                                    DELETE FROM customers
+                                                    WHERE custid = ?
                                                     """;
     private static final String UPDATE_CUSTOMER = """
-                                                    UPDATE categories
-                                                    SET  categoryname = ?, description = ?
-                                                    WHERE categoryid = ?
+                                                    UPDATE customers
+                                                    SET  custid = ?, companyname = ?, contactname = ?, 
+                                                    contacttitle = ?, adress = ?, city = ?, region = ?, postalcode = ?, 
+                                                    country = ?, phone = ?, fax = ?
+                                                    WHERE custid = ?
                                                     """;
     private static final String INSERT_CUSTOMER = """
                                                         INSERT INTO categories
@@ -67,15 +74,17 @@ public class JdbcCustomerRepositery implements CustomerRepositery{
     public Iterable<Customer> getByNameLike(String part) throws DataException {
         try(
             Connection c = ConnectionUtils.createConnection();
-            PreparedStatement ps = c.prepareStatement(ALL_CATEGORIES_NAME_LIKE);
+            PreparedStatement ps = c.prepareStatement(ALL_CUSTOMER_NAME_LIKE);
         ){
                 ps.setString(1, "%"+part+"%");
                 try(ResultSet rs = ps.executeQuery()){
-                    Collection<Category> cats = new ArrayList<>();
+                    Collection<Customer> custs = new ArrayList<>();
                     while(rs.next()){
-                        cats.add(new Category(rs.getInt("categoryid"), rs.getString("categoryname"), rs.getString("description")));
+                        custs.add(new Customer(rs.getString("companyname"), rs.getString("contactname"), 
+                        rs.getString("contacttitle"), rs.getString("adress"), rs.getString("city"), rs.getString("region"), 
+                        rs.getString("postalcode"), rs.getString("country"), rs.getString("phone"), rs.getString("fax")));
                     }
-                    return cats;
+                    return custs;
                 }
                 
             } catch(SQLException e){
@@ -87,19 +96,20 @@ public class JdbcCustomerRepositery implements CustomerRepositery{
     public Optional<Customer> findById(int id) throws DataException {
         try(
             Connection c = ConnectionUtils.createConnection();
-            PreparedStatement ps = c.prepareStatement(CATEGORY_BY_ID);
+            PreparedStatement ps = c.prepareStatement(CUSTOMER_BY_ID);
         ){
             ps.setInt(1, id);
             try(ResultSet rs = ps.executeQuery()){
                 if(rs.next()){
-                    return Optional.of(new Category(rs.getInt("categoryid"),
-                    rs.getString("categoryname"), rs.getString("description")));
+                    return Optional.of(new Customer(rs.getString("companyname"), rs.getString("contactname"), 
+                    rs.getString("contacttitle"), rs.getString("adress"), rs.getString("city"), rs.getString("region"), 
+                    rs.getString("postalcode"), rs.getString("country"), rs.getString("phone"), rs.getString("fax")));
                 } else {
                     return Optional.empty();
                 }
             }
         }catch(SQLException e){
-            throw new DataException("Errore nella ricerca di categorie per id", e);
+            throw new DataException("Errore nella ricerca di cliente per id", e);
         }
     }
 
@@ -113,25 +123,26 @@ public class JdbcCustomerRepositery implements CustomerRepositery{
             int n = ps.executeUpdate();
             return n == 1;
         }catch(SQLException e){
-            throw new DataException("Errore nell'eliminazione di categorie per id", e);
+            throw new DataException("Errore nell'eliminazione di cliente per id", e);
         }
     }
 
     @Override
-    public Optional<Customer> update(Customer newCategory) throws DataException {
-        Optional<Category> oldCat = findById(newCategory.getId());
-        if(oldCat.isEmpty()){
+    public Optional<Customer> update(Customer newCustomer) throws DataException {
+        Optional<Customer> oldCust = findById(newCustomer.getId());
+        if(oldCust.isEmpty()){
             return Optional.empty();
         }
         try(
             Connection c = ConnectionUtils.createConnection();
-            PreparedStatement ps = c.prepareStatement(UPDATE_CATEGORY);
+            PreparedStatement ps = c.prepareStatement(UPDATE_CUSTOMER);
         ){
-            ps.setString(1, newCategory.getName());
-            ps.setString(2, newCategory.getDescription());
-            ps.setInt(3, newCategory.getId());
+            ps.setString(1, newCustomer.getCompanyName());
+            ps.setString(2, newCustomer.getContactName());
+            ps.setString(3, newCustomer.getContactName());
+            ps.setInt(11, newCustomer.getId());
             ps.executeUpdate();               
-            return oldCat;
+            return oldCust;
         }catch(SQLException e){
             throw new DataException("Errore nella modifica", e);
         }
@@ -141,7 +152,7 @@ public class JdbcCustomerRepositery implements CustomerRepositery{
     public Customer create(Customer category) throws DataException{
         try(
             Connection c = ConnectionUtils.createConnection();
-            PreparedStatement ps = c.prepareStatement(INSERT_CATEGORY, Statement.RETURN_GENERATED_KEYS); 
+            PreparedStatement ps = c.prepareStatement(INSERT_CUSTOMER, Statement.RETURN_GENERATED_KEYS); 
         ){
             ps.setString(1, category.getName());
             ps.setString(2, category.getDescription());
@@ -149,7 +160,7 @@ public class JdbcCustomerRepositery implements CustomerRepositery{
             ResultSet rs = ps.getGeneratedKeys();
             if(rs.next()){
                 int key = rs.getInt(1);
-                category.setCategoryId(key);
+                category.setCustomerId(key);
             }
             return category;
         }catch(SQLException e){
